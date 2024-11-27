0921cbc28fc780fb600619ba3f0f4e9fed5a9a24
Eduardo Henrique <eduardo.hor@outlook.com>
2024-11-27 10:37:48 -0300

    adicionando cpt

--- mv-slider.php
+++ mv-slider.php
@@ -40,6 +40,9 @@ if (!class_exists('MV_Slider')) {
         function __construct()
         {
             $this->define_constants();
+
+            require_once(MV_SLIDER_PATH . 'post-types/class.mv-slider-cpt.php');
+            $MV_Slider_Post_Type = new MV_Slider_Post_Type();
         }
 
         public function define_constants()
@@ -52,6 +55,7 @@ if (!class_exists('MV_Slider')) {
         public static function activate()
         {
             update_option('rewrite_rules', '');
+            unregister_post_type('mv-slider');
         }
 
         public static function deactivate()
